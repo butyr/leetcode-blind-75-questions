b9e928e1ff23eedfacd4a7f5deeb6d3111882833
butyr <L.Butyrev@gmx.de>
2021-10-17 11:32:52 +0200

    cleaned up code

--- longest_palindromic_substring/solution.py
+++ longest_palindromic_substring/solution.py
@@ -22,13 +22,13 @@ class Solution:
 
         return max_palindrome
 
-    def get_palindrome_substring(self, s: str, idx: int) -> str:
-        curr_idx_left, curr_idx_right = self.get_idxs_around_center(s, idx)
+    def get_palindrome_substring(self, input_string: str, idx: int) -> str:
+        curr_idx_left, curr_idx_right = self.get_idxs_around_center(input_string, idx)
         curr_substring = ""
 
-        while curr_idx_left >= 0 and curr_idx_right < len(s):
-            if s[curr_idx_left] == s[curr_idx_right]:
-                curr_substring = s[curr_idx_left: curr_idx_right + 1]
+        while curr_idx_left >= 0 and curr_idx_right < len(input_string):
+            if input_string[curr_idx_left] == input_string[curr_idx_right]:
+                curr_substring = input_string[curr_idx_left: curr_idx_right + 1]
             else:
                 break
 
@@ -38,13 +38,13 @@ class Solution:
         return curr_substring
 
     @staticmethod
-    def get_idxs_around_center(s: str, idx: int) -> Tuple[int, int]:
+    def get_idxs_around_center(input_string: str, idx: int) -> Tuple[int, int]:
         """
         Computes the starting indices around the palindrome center.
         Sometimes the center is not just a single character.
         For that case the indices need to be adjusted.
 
-        :param s:
+        :param input_string:
         :param idx:
         :return:
         """
@@ -52,10 +52,10 @@ class Solution:
         curr_idx_left = idx
         curr_idx_right = idx
 
-        while curr_idx_left - 1 >= 0 and s[curr_idx_left - 1] == s[idx]:
+        while curr_idx_left - 1 >= 0 and input_string[curr_idx_left - 1] == input_string[idx]:
             curr_idx_left -= 1
 
-        while curr_idx_right + 1 < len(s) and s[curr_idx_right + 1] == s[idx]:
+        while curr_idx_right + 1 < len(input_string) and input_string[curr_idx_right + 1] == input_string[idx]:
             curr_idx_right += 1
 
         return curr_idx_left, curr_idx_right
